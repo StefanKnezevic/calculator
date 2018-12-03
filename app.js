71b2b368db5a473ddb9817202401d7f0119015d1
Stefan Knezevic <knezknezevicstefan@gmail.com>
2018-12-03 09:54:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+    
+
+    e.preventDefault();
+});
+
+
+// Calculate Results - zato sto je submit dodajemo preventDefault
+function calculateResults() {
+    console.log('Calculating...');
+    // UI vars
+    const amount = document.getElementById('amount');
+    const intrest = document.getElementById('intrest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+    const principal = parseFloat(amount.value); // Prebacujemo u decimal sa parseFloat;
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x-1);
+    
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
+    } else {
+        showError('Please check yours numbers');
+    }
+};
+
+// showError 
+function showError(error) {
+    document.getElementById('results').style.display = 'none';
+    // Create a div, add className and appendChild(textNode)
+    const errorDiv = document.createElement('div');
+
+    // get element
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error before heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 second
+    setTimeout(clearError, 3000); // 3000 ms 
+
+    function clearError(){
+        document.querySelector('.alert').remove();
+    }
+};
+
